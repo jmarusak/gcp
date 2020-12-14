3eb96c8a6158a87a01aadf4ee1205de85603f39e
jmarusak <code@work.com>
2020-12-14 17:15:08 +0000

    project name changed

--- bigquery/client_sdk_bq.py
+++ bigquery/client_sdk_bq.py
@@ -1,14 +1,15 @@
-PROJECT='projectx-287701' 
+PROJECT='projectx-dbm' 
 BUCKET='projectx-beam-pipeline'
-REGION='us-central1'
-
+REGION='northamerica-northeast1'
 
 from google.cloud import bigquery
+
 bq = bigquery.Client(project=PROJECT)
 query = """
 SELECT duration
 FrOM `bigquery-public-data.london_bicycles.cycle_hire`
 WHERE start_station_id = 708
 """
+
 df = bq.query(query, location='EU').to_dataframe()
 print(df.describe())
